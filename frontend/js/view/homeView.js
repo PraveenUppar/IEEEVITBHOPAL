68ab774329c0910bd663abe74f0ebab8d9bb13e8
tejasva-087 <khandelwaltejasva@gmail.com>
2024-08-07 00:37:26 +0530

    Added the js back

--- frontend/js/view/homeView.js
+++ frontend/js/view/homeView.js
@@ -3,13 +3,40 @@ class HomeView {
 
   constructor() {
     this.handelNavClick();
+    this.handelMainOptionClick();
+  }
+
+  #changeActiveSection(target) {
+    this.#activeSection.classList.remove("nav-items--active");
+    document
+      .getElementById(this.#activeSection.dataset.section)
+      .classList.add("hidden");
+
+    this.#activeSection = target;
+
+    this.#activeSection.classList.add("nav-items--active");
+    document
+      .getElementById(this.#activeSection.dataset.section)
+      .classList.remove("hidden");
   }
 
   handelNavClick() {
     const parentEl = document.querySelector(".nav-list");
     parentEl.addEventListener("click", (e) => {
       const target = e.target.closest(".nav-items");
-      console.log(target);
+      if (!target) return;
+      this.#changeActiveSection(target);
+    });
+  }
+
+  handelMainOptionClick() {
+    const parentEl = document.querySelector(".main-options-container");
+    parentEl.addEventListener("click", (e) => {
+      const target = e.target.closest(".main-options-box-text-btn");
+      if (!target) return;
+      this.#changeActiveSection(
+        document.querySelector(`.${target.dataset.section}`)
+      );
     });
   }
 }
