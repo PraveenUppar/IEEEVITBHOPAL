073c583fc8785af6b266dd2d5f48e9853c5a9685
PraveenUppar <144151782+PraveenUppar@users.noreply.github.com>
2024-08-06 16:51:19 +0530

    Created Endpoints for Login and Register

--- Backend/index.js
+++ Backend/index.js
@@ -2,15 +2,76 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const User = require("./Models/User.js");
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
 require("dotenv").config();
 
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
+const secret = bcrypt.genSaltSync(10);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:1234",
+  })
+);
+
 mongoose.connect(
   "mongodb+srv://praveenuppar718:AEzmZXInlgBKWtP7@uni-help.owbnnyr.mongodb.net/?retryWrites=true&w=majority&appName=Uni-Help"
 );
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    jwt.sign({ email, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        email,
+      });
+    });
+  } else {
+    res.status(400).json("Wrong credentials");
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
 
 app.listen(7000, () => {
   console.log("server running on localhost:7000");
