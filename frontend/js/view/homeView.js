1cc47e0bf8f1d966451b22ed135bcfcaa5e37e36
tejasva-087 <khandelwaltejasva@gmail.com>
2024-08-07 07:05:54 +0530

    Added the login form element

--- frontend/js/view/homeView.js
+++ frontend/js/view/homeView.js
@@ -4,6 +4,8 @@ class HomeView {
   constructor() {
     this.handelNavClick();
     this.handelMainOptionClick();
+    this.handleLoginBtnClick();
+    this.handelFormExitClick();
   }
 
   #changeActiveSection(target) {
@@ -37,6 +39,96 @@ class HomeView {
       this.#changeActiveSection(
         document.querySelector(`.${target.dataset.section}`)
       );
+    });
+  }
+
+  // btn-sign-in
+  //  btn-sign-up
+
+  renderForm(type) {
+    document.getElementById("sign-in-form").classList.remove("hidden");
+    const formParent = document.querySelector(".main-form-box");
+    const html = `
+            <form action="#" class="form">
+              <p class="main-form-heading">
+                ${type == "btn-sign-in" ? "Sign In" : "Register"}
+              </p>
+              <div class="form-box margin-bottom-med ${
+                type == "btn-sign-in" ? "hidden" : ""
+              }">
+                <label for="name"
+                  ><i class="ph-fill ph-user-circle form-icon"></i
+                ></label>
+                <input
+                  type="text"
+                  id="name"
+                  class="form-input"
+                  placeholder="Full name"
+                  required
+                />
+                <label for="name" class="form-label-text">Full name</label>
+                <i class="ph-fill ph-warning form-icon-sml form-warning"></i>
+              </div>
+              <div class="form-box">
+                <label for="email">
+                  <i class="ph-fill ph-envelope form-icon"></i>
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  class="form-input"
+                  placeholder="E-mail"
+                  required
+                />
+                <label for="name" class="form-label-text">E-mail</label>
+                <i class="ph-fill ph-warning form-icon-sml form-warning"></i>
+              </div>
+              <div class="form-box margin-bottom-med">
+                <label for="password">
+                  <i class="ph-fill ph-lock-key-open form-icon"></i>
+                </label>
+                <input
+                  type="Password"
+                  id="password"
+                  class="form-input"
+                  placeholder="Password"
+                  required
+                />
+                <label for="name" class="form-label-text">Password</label>
+                <i class="ph-fill ph-warning form-icon-sml form-warning"></i>
+              </div>
+              <button class="btn form-btn">Sign Up</button>
+            </form>
+            <button class="main-form-cancel-btn btn">
+              <i class="ph ph-x"></i>
+            </button>
+    `;
+    formParent.insertAdjacentHTML("afterbegin", html);
+    formParent.classList.remove("hidden");
+  }
+
+  removeForm() {
+    document.getElementById("sign-in-form").classList.add("hidden");
+    const formParent = document.querySelector(".main-form-box");
+    formParent.innerHTML = "";
+    formParent.classList.add("hidden");
+  }
+
+  handleLoginBtnClick() {
+    const parentEl = document.querySelector(".nav-extras");
+    parentEl.addEventListener("click", (e) => {
+      const target = e.target.closest(".nav-extras-btn");
+      if (!target) return;
+      this.renderForm(target.dataset.btnType);
+    });
+  }
+
+  handelFormExitClick() {
+    const parentEl = document.getElementById("sign-in-form");
+    parentEl.addEventListener("click", (e) => {
+      const btnTarget = e.target.closest(".main-form-cancel-btn");
+      if (!btnTarget) return;
+      this.removeForm();
     });
   }
 }
